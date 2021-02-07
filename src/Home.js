3d91f59e6a9ad985448ac0453ab769b513428eab
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-07 16:19:12 +0000

    Practised console logging click events

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,16 @@
 const Home = () => {
 
-  const handleClick = () => {
-    console.log('hello, ninjas');
+  const handleClick = (e) => {
+    console.log('hello, ninjas', e);
   }
-  const handleClickAgain = (name) => {
-    console.log('hello ' + name);
+  const handleClickAgain = (name, e) => {
+    console.log('hello ' + name, e.target);
   }
   return ( 
     <div className="home">
       <h2>Homepage</h2>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
+      <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
     </div>
    );
 }

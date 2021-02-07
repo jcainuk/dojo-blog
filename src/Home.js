54cc4c67b94296fd2b7ada241bcd8d8e98bcce67
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-07 16:14:41 +0000

    Practised nesting anonymous functions for click events on buttons in Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+
+  const handleClick = () => {
+    console.log('hello, ninjas');
+  }
+  const handleClickAgain = (name) => {
+    console.log('hello ' + name);
+  }
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
     </div>
    );
 }

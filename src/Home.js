63200bc113d3936831341ccd97b9fa37cf46146d
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-07 16:45:29 +0000

    Practised using reactive values by using State (useState hook)

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,20 @@
-const Home = () => {
+import { useState } from 'react';
 
-  const handleClick = (e) => {
-    console.log('hello, ninjas', e);
+const Home = () => {
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    setName('luigi');
+    setAge(30);
   }
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-  }
+  
   return ( 
     <div className="home">
       <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
       <button onClick={handleClick}>Click me</button>
-      <button onClick={(e) => handleClickAgain('Mario', e)}>Click me again</button>
+    
     </div>
    );
 }

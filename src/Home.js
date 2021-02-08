135a5eda1c3b41aceef53f8dd2dc584cc978a462
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-08 02:19:03 +0000

    by using a dependency array with a value in as a second argument of the useEffect function we can tell the function to only fire when that particular value is modified

--- src/Home.js
+++ src/Home.js
@@ -8,24 +8,29 @@ const Home = () => {
     {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
     {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
   ]);
+
+  const [name, setname] =useState('Mario');
   
   const handleDelete = (id) => {
     const newBlogs = blogs.filter(blog => blog.id !== id);
     setBlogs(newBlogs);
   }
-/* By passing useEffect an array [] as a second argument after the callback function
-You can ensure that the useEffect() function will only run on the first render.
-
-There by doing so the function willl only run once. */
+/* By passing useEffect function a dependency array with a value like 
+[name ] as a second argument 
+after the callback function
+you can ensure that the useEffect() function will only run on the first render
+and when that dependency value is changed.*/
 
   useEffect(() => {
     console.log('use effect ran');
-    console.log(blogs);
-  }, []);
+    console.log(name);
+  }, [ name ]);
 
   return ( 
     <div className="home">
       <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete }/> 
+      <button onClick={() => setname('Luigi')}>Change Name</button>
+      <p>{ name }</p>
       
     </div>
    );

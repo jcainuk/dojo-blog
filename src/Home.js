874df933d8d1c9667a43230e63cc35e4c8322fad
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-08 13:52:34 +0000

    Built a conditional loading message with conditional loading div element using a useState

--- src/Home.js
+++ src/Home.js
@@ -5,31 +5,28 @@ const Home = () => {
   // Blogs initial state is null (a falsey value)
 
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
-  const [name, setname] =useState('Mario');
-  
 
   useEffect(() => {
-    fetch('http://localhost:8000/blogs')
+    // Don't use SetTimeout in real life. This was just to see the loading message in browser.
+    setTimeout(() => {fetch('http://localhost:8000/blogs')
     .then(res => {
       return res.json()
     })
     .then( data => {
       setBlogs(data);
-    });
+      setIsPending(false);
+      });
+    }, 1000);
   }, []);
+    
 
   return ( 
     <div className="home">
-
-{/* 1. Comments inside children need to be in braces.
-    2. Below we are using Javascript logic. Everything to the left of '&&' won't run
-     if it is null (a falsey value). It only runs is a truthy value. 
-    3. This is how we conditionally output parts of a template */}
-
-      {blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
-      <button onClick={() => setname('Luigi')}>Change Name</button>
-      <p>{ name }</p>
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={ blogs } title="All Blogs!" /> }
+      
       
     </div>
    );

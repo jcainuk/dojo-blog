d0152c635ad052b25c38c1227da8e38766ef2ac0
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-08 20:45:46 +0000

    Built input fields that track what a user types into them, using controlled inputs in the Create component

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Mario');
+
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
@@ -7,18 +13,28 @@ const Create = () => {
         <input 
         type="text"
         required
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         />
         <label>Blog Body:</label>
         <textarea
         required
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
         ></textarea>
         <label>Blog Author:</label>
-        <select>
+        <select
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        >
           <option value="Mario">Mario</option>
           <option value="Yoshi">Yoshi</option>
         </select>
         <button>Add Blog</button>
       </form>
+      <p>{title}</p>
+      <p>{body}</p>
+      <p>{author}</p>
     </div>
   );
 }

08fd0349d5daf48c75bcf285b2dfb89c2bba9db0
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-08 01:47:36 +0000

    Practised passing functions into other components as props to delete blogs from the blog list

--- src/Home.js
+++ src/Home.js
@@ -3,17 +3,22 @@ import BlogList from './BlogList';
 
 const Home = () => {
   
-  const [blogs, setBlog] = useState([
+  const [blogs, setBlogs] = useState([
     {title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
     {title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
     {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
   ]);
   
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter(blog => blog.id !== id);
+    setBlogs(newBlogs);
+
+  }
+
   return ( 
     <div className="home">
-      <BlogList blogs={ blogs } title="All Blogs!"/> 
-      <BlogList blogs={ blogs.filter((blog) => blog.author === 'Mario' ) } title="Mario's Blogs!"/> 
-    
+      <BlogList blogs={ blogs } title="All Blogs!" handleDelete={ handleDelete }/> 
+      
     </div>
    );
 }

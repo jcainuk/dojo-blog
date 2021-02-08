3e6b234124c64d16c991eeae3abc48b54a5cfa78
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2021-02-08 20:28:05 +0000

    Added a form template to the Create.js component and styled it

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,23 @@ const Create = () => {
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input 
+        type="text"
+        required
+        />
+        <label>Blog Body:</label>
+        <textarea
+        required
+        ></textarea>
+        <label>Blog Author:</label>
+        <select>
+          <option value="Mario">Mario</option>
+          <option value="Yoshi">Yoshi</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
